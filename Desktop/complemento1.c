ef02bfdb27af3c2867769b4e2fa868547d36613e
Ax Angel <angel_r_m2001@hotmail.com>
2015-10-06 22:12:25 -0500

    Decimal a complemento 1

---
+++ Desktop/complemento1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+	int i,j,n,a[1000000];
+	printf("Igrese un numero entero:  \n");
+	scanf("%d",n);
+	while(n>0){
+		a[i]=n%2;
+		n/=2;
+		i++
+	}
+
+	for(j=i-1;j>=0;j--){
+		if(a[j]==0)
+			a[j]=1;
+		else
+			a[j]=0;
+	}
+
+	for(j=i-1;j>=0;j--)
+		printf("%d",a[j]);
+	printf("\n");
+	return 0;
+}
